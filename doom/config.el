89caa629882a9566f6210cd95ecfbbaabc07ac7d
Peter Lewis <pete@muddygoat.org>
2019-11-10 10:07:46 +0000

    switch to solarized dark for doom

--- doom/config.el
+++ doom/config.el
@@ -9,7 +9,7 @@
 
 ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
 ;; may have their own settings.
-(load-theme 'doom-vibrant t)
+(load-theme 'doom-solarized-dark t)
 
 ;; Enable flashing mode-line on errors
 (doom-themes-visual-bell-config)
